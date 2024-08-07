48785f6a2130cbf9fd0e44d6424246cec8adee96
Ahana Deb <ahanadeb@Ahanas-MacBook-Air.local>
2024-08-07 13:37:54 +0530

    some progress

---
+++ env/get_ao.py
@@ -0,0 +1,16 @@
+def get_a(q):
+    return q.c[0]
+
+
+def get_o(q):
+    if len(q.c) > 1:
+        return q.c[1]
+    else:
+        obs = ' '
+        return obs
+
+
+def get_r(q):
+    if len(q.c) > 1:
+        return q.c[2]
+    return 0
--- learn_cyclic_pdfa.py
+++ learn_cyclic_pdfa.py
@@ -1,10 +1,10 @@
 from src.pdfa import PDFA
 from src.utils_pdfa.adact_utils import *
 from src.utils_pdfa.State import State
-
+from env.get_ao import get_r, get_o, get_a
 
 def get_initial_candidates(D, first_obs, A, pdfa, Q_c, Q_prev_list):
-    Q_prev_list[0]=[]
+    Q_prev_list[0] = []
     candidates = get_first_obs(first_obs)
     print("candidates", candidates)
     for i in range(candidates.shape[0]):
@@ -29,45 +29,48 @@ def get_max_qao(Q):
     return state, t
 
 
-def remove_candidate_from_Q(Q, Q_c, q, t):
+def remove_candidate_from_Q(Q, Q_c, Q_prev_list, q, t):
     Q_c.remove(q)
+    del Q_prev_list[t][Q[t].index(q)]
     Q[t].remove(q)
-    #have to remove from Q_prev as well
 
     return Q, Q_c
 
 
-def add_state_to_Q_final(Q_final, t, q, Q_prev_list):
+def add_state_to_Q_final(Q_final, Q, t, q, pdfa, Q_prev_list):
     if Q_final[t] == None:
         Q_final[t] = []
     Q_final[t].append(q)
-
-    return Q_final
+    pdfa.add_transition(Q_prev_list[t][Q[t].index(q)], get_a(q), q, get_o(q), get_r(q))
+    #remove candidate from list
+    return Q_final, Q
 
 
 def learn_cyclic_pdfa(D, first_obs, A, a_dict, K, H):
     Q = [None] * H  #list of lists for all candidates
-    Q_final = [None] * H # list of lists for final safe states
-    Q_prev_list = [None]*H
-    Q_c = []  #Full candidate list
-    #add initial pdfa state
+    Q_final = [None] * H  # list of lists for final safe states
+    Q_prev_list = [None] * H
+    Q_c = []  # Full candidate list
+    # add initial pdfa state
     pdfa = PDFA(D, A, a_dict)
     q0 = pdfa.initial_state
     q0.hist = list(range(K + 1))
-    #add first list of candidates
+    # add first list of candidates
     Q_c, Q_prev_list = get_initial_candidates(D, first_obs, A, pdfa, Q_c, Q_prev_list)
     Q[0] = Q_c
-    #after this Q_c becomes the list of all candidates
+    # after this Q_c becomes the list of all candidates
     while Q_c:
-        #get most occuring qao across all times
-        #the time t wouldn't matter in the first case, you're not supposed to have all the q states from the start
+        # get most occurring qao across all times
+        # the time t wouldn't matter in the first case, you're not supposed to have all the q states from the start
         q_max, t_max = get_max_qao(Q)
-        Q, Q_c = remove_candidate_from_Q(Q, Q_c, q_max, t_max)
-        #get similar states by comparing to all times till t_max
+        # remove it after promoting
+       # Q, Q_c = remove_candidate_from_Q(Q, Q_c, Q_prev_list, q_max, t_max)
+        # get similar states by comparing to all times till t_max
         similar = []
-        #promote if no similar
+        # promote if no similar
         if not similar:
-            Q_final = add_state_to_Q_final(Q_final, t_max, q_max, pdfa, Q_prev_list)
-
-
+            Q_final = add_state_to_Q_final(Q_final, Q, t_max, q_max, pdfa, Q_prev_list)
+            Q, Q_c = remove_candidate_from_Q(Q, Q_c, Q_prev_list, q_max, t_max)
+            # add new candidates stemming from this state
+            add_new_candidates(q_max,D, ) #how to know where the next candidate comes from
     return pdfa
